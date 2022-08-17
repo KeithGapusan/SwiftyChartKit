0c87eb9dc1ef15a67ece0a2d7b1543bc2135d37f
Keith <krsgapusan@gmail.com>
2022-08-17 10:51:47 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftyChartKit
+
+A description of this package.
---
+++ Tests/SwiftyChartKitTests/SwiftyChartKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyChartKit
+
+final class SwiftyChartKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyChartKit().text, "Hello, World!")
+    }
+}
